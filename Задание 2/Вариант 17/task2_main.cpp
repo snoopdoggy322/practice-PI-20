9f95cb7fcce573101bb2ca4087e7f3f50fce0147
snoopdoggy322 <snoopdoggy322@gmail.com>
2021-07-02 11:56:10 +0300

    Add tests

--- Задание 2/Вариант 17/task2_main.cpp
+++ Задание 2/Вариант 17/task2_main.cpp
@@ -14,7 +14,7 @@ class DateTime{
     int minutes;
     int seconds;
     
- public:   
+ public:
     DateTime(int _year,int _month,int _day,int _hour,int _minutes,int _seconds){
        year=_year;
        month=_month;
@@ -27,30 +27,15 @@ class DateTime{
     long differenceInSeconds(DateTime secondDate){
         return seconds+(minutes*60)+(hour*60*60)+(day*24*60*60)+(month*30*24*60*60)+(year*365*24*60*60);
     }
-    DateTime fromSeconds(long s){
-        int year =s/60/60/24/365;
-        s-=year;
-        int month =s/30/24/60/60;
-        s-=month;
-        int day =s/24/60/60;
-        s-=day;
-        int hour =s/60/60;
-        s-=hour;
-        int minutes =s/60;
-        s-=minutes;
-        int seconds =s;
-        s-=seconds;
-        return DateTime(
-            year,
-            month,day,hour,minutes,seconds
-            );
-    }
-};
 
+};
+    int toDays(long s){
+       return s/60/86839;
+    }
 int main()
 {
-     DateTime newYear = DateTime(2022,12,31,12,59,59);
+     DateTime newYear = DateTime(2021,12,31,12,59,59);
     DateTime date = DateTime(2021,6,29,17,40,55);
-     std::cout << DateTime.fromSeconds(date.differenceInSeconds(newYear)) << std::endl;
+     std::cout <<toDays(date.differenceInSeconds(newYear)) << std::endl;
     return 0;
 }
