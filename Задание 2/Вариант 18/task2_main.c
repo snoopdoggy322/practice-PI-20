179c25632b2b0e98b3b09c72ec1e6c82ecd509cd
IGRIDI <lin8ro@gmail.com>
2021-07-02 14:31:09 +0300

    Create task2_main.c

---
+++ Задание 2/Вариант 18/task2_main.c
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    struct date
+    {
+        int day;
+        int month;
+        int year;
+    };
+
+    int n, number;
+    date* dates;
+    n = 2;
+    dates = new date[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the day for " << i + 1 << " date: ";
+        cin >> dates[i].day;
+        cout << endl;
+
+        cout << "Enter the month for " << i + 1 << " date: ";
+        cin >> dates[i].month;
+        cout << endl;
+
+        cout << "Enter the year for " << i + 1 << " date: ";
+        cin >> dates[i].year;
+        cout << endl;
+
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+
+        if ((dates[i].year <= dates[i + 1].year) && (dates[i].month <= dates[i + 1].month))
+        {
+            number = (dates[i+1].year - dates[i].year)*12 + dates[i + 1].month - dates[i].month;
+            cout << number << endl;
+
+        }
+        else return 0;
+
+    }
+    return 0;
+    system("pause");
+}
