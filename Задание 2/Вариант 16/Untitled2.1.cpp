eb1e16060e25111effa49c2aade2948e730143a9
ValenoG <86828784+ValenoG@users.noreply.github.com>
2021-07-04 15:15:14 +0300

    Add files via upload

---
+++ Задание 2/Вариант 16/Untitled2.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+ 
+void IncTime(int &,int &,int &,int);
+ 
+int main()
+{
+  int hours, minutes, seconds;
+    printf("Vvedite vremya:");
+    scanf("%u %u %u", &hours, &minutes, &seconds);
+    IncTime(hours,minutes,seconds,1);
+    printf("%d:%d:%d",hours,minutes,seconds);
+    return 0;
+}
+ 
+void IncTime(int &H,int &M,int &S,int T){
+    int tm= 3600*H+60*M+S+60;
+    H = tm/3600;
+    tm %= 3600;
+    M = tm/60;
+    S = tm%60;
+}
